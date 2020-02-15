88f8222609767d4ebf3726e6e5880c677f5cfcf9
jithin0000 <mbjithinbabu@gmail.com>
2020-02-15 11:21:56 +0530

    user model added

--- customuser/models.py
+++ customuser/models.py
@@ -4,5 +4,55 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 
 class MyUserManager(BaseUserManager):
-    """ user manager for class "
-    pass
+    """ user manager for class """
+
+    def create_user(self, email, username, password=None):
+        """ user creating method """
+
+        if not email:
+            raise ValuError("User must have an email address ")
+
+        user = self.model(
+                
+                email = email, username=username
+                )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None):
+        """ create superuser functions """
+
+        user = self.create_user(email, username, password)
+
+        user.is_admin=True
+        user.is_staff=True
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(AbstractBaseUser):
+    """ overriding base user """
+
+    email = models.EmailField(verbose_name='Email Address', max_length=255, unique=True)
+    username = models.CharField(max_length=255)
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS= ['username']
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
