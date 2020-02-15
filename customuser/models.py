716248033b8ce8e5141e66b84b6ffd0a67cc6982
jithin0000 <mbjithinbabu@gmail.com>
2020-02-15 11:06:27 +0530

    initial commit

---
+++ customuser/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+# Create your models here.
+
+
+class MyUserManager(BaseUserManager):
+    """ user manager for class "
+    pass
